bda5ebdf2ee6cb47a45c8c61579cca84abd17246
Abel <ablwang@ucdavis.edu>
2015-11-12 11:36:54 -0800

    Fix merge conflict

--- public/concerts.js
+++ public/concerts.js
@@ -1,5 +1,5 @@
-angular.module('beatssounds.concerts', [])
-  .controller('concertController', function($scope, auth, time, space) {
+angular.module('beatssounds.concerts', ['ui.bootstrap'])
+  .controller('concertController', function ($scope, auth, time, space) {
 
     $scope.parseMonth = function(date) {
       return time.parseMonth(date);
@@ -31,9 +31,10 @@ angular.module('beatssounds.concerts', [])
     $scope.getConcerts = function() {
       this.isLoading = true;
       auth.getConcerts().then(function(resp) {
-        console.log(resp);
         $scope.isLoading = false;
         $scope.data = resp;
+        $scope.paginate();
+        // $scope.pageCounter();
       })
     };
 
@@ -45,4 +46,27 @@ angular.module('beatssounds.concerts', [])
       $scope.getConcerts();
     }
 
+    /// PAGINATION ==========================================
+    $scope.itemsPerPage = 4
+    $scope.currentPage = 1;
+
+    $scope.pageCount = function () {
+      return Math.ceil($scope.data.length / $scope.itemsPerPage);
+    };
+
+    $scope.paginate = function () {
+      $scope.totalItems = $scope.data.length;
+      $scope.$watch('currentPage + itemsPerPage', function() {
+        var begin = (($scope.currentPage - 1) * $scope.itemsPerPage);
+        var end = begin + $scope.itemsPerPage;
+        $scope.filteredEvents = $scope.data.slice(begin, end);
+      
+        var counter = $scope.data.length / $scope.itemsPerPage;
+        if ($scope.currentPage <= counter) {
+          $scope.showing = $scope.itemsPerPage;
+        } else {
+          $scope.showing = $scope.data.length;
+        }
+      });
+    };
   });
