f5b76155983c08c7a704bce58eb5e5f7e30bdafa
Hack Reactor Students <accounts@hackreactor.com>
2015-11-11 21:35:44 -0800

    (feat) Implementing db helper functions

--- server/db/dbHelper.js
+++ server/db/dbHelper.js
@@ -1,7 +1,13 @@
 module.exports.insertHandler = function(concerts, callback){
+  console.log(JSON.stringify(concerts.event[0]));
+  // concerts.forEach(function(concert){
 
-
+  // });
   
 
   callback(concerts);
 };
+
+
+
+
